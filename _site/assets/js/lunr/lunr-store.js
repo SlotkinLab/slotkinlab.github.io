3c3580e35a3f3c50af7d6b05deeed0961df019f0
Saima Shahid <saima@Saimas-iMac.local>
2020-05-05 14:29:41 -0500

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,73 +1,73 @@
 var store = [{
-        "title": "Andrea McCue",
+        "title": "Yu-Hung Hung",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/andrea-mccue/",
-        "teaser": "http://localhost:4000/assets/images/people/ADM.jpg"
-      },{
-        "title": "Diego Cuerda-Gil",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/diego-cuerdagil/",
-        "teaser": "http://localhost:4000/assets/images/people/DCG-3.jpg"
-      },{
-        "title": "Hayden Payne",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/hayden-payne/",
-        "teaser": "http://localhost:4000/assets/images/people/HAP.jpg"
-      },{
-        "title": "Kaushik Panda",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/kaushik-panda/",
-        "teaser": "http://localhost:4000/assets/images/people/KP.jpg"
-      },{
-        "title": "Lauren McLain",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/lauren-mclain/",
-        "teaser": "http://localhost:4000/assets/images/people/LLM.jpg"
-      },{
-        "title": "Meredith Sigman",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/meredith-sigman/",
-        "teaser": "http://localhost:4000/assets/images/people/MJS.jpg"
-      },{
-        "title": "Missy Rung-Blue",
-        "excerpt":"Missy began her career in the for-profit sector and spent 18 years there gaining knowledge and experience with budgeting, project planning, financial reporting, project management, information technology, and team leadership. After 18 years, she decided that she wanted to take these skills to the non-profit sector because she believes that...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/people/missy-rungblue/",
-        "teaser": "http://localhost:4000/assets/images/people/MRW.jpg"
+        "url": "http://localhost:4000/people/1-hung-yuhung/",
+        "teaser": "http://localhost:4000/assets/images/people/YHH.jpg"
       },{
         "title": "Peng Liu",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/peng-liu/",
+        "url": "http://localhost:4000/people/1-liu-peng/",
         "teaser": "http://localhost:4000/assets/images/people/PL.jpg"
+      },{
+        "title": "Andrea McCue",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/1-mccue-andrea/",
+        "teaser": "http://localhost:4000/assets/images/people/ADM.jpg"
+      },{
+        "title": "Kaushik Panda",
+        "excerpt":"My journey so far: I joined Dr. Keith Slotkin’s lab as a graduate student at The Ohio State University in 2012. Over the years I enjoyed developing and leading the bioinformatics segment of the lab and graduated with a PhD in Molecular Genetics. Towards the end of my graduate career...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/1-panda-kaushik/",
+        "teaser": "http://localhost:4000/assets/images/people/KP.jpg"
       },{
         "title": "Saima Shahid",
         "excerpt":"Saima is a Simons fellow for the Life Science Research Foundation.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/saima-shahid/",
+        "url": "http://localhost:4000/people/1-shahid-saima/",
         "teaser": "http://localhost:4000/assets/images/people/SS.jpg"
+      },{
+        "title": "Diego Cuerda-Gil",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/2-cuerdagil-diego/",
+        "teaser": "http://localhost:4000/assets/images/people/DCG-3.jpg"
       },{
         "title": "Seth Edwards",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/seth-edwards/",
+        "url": "http://localhost:4000/people/2-edwards-seth/",
         "teaser": "http://localhost:4000/assets/images/people/SE.jpg"
+      },{
+        "title": "Meredith Sigman",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/2-sigman-meredith/",
+        "teaser": "http://localhost:4000/assets/images/people/MJS.jpg"
+      },{
+        "title": "Lauren McLain",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/3-mclain-lauren/",
+        "teaser": "http://localhost:4000/assets/images/people/LLM.jpg"
+      },{
+        "title": "Hayden Payne",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/people/3-payne-hayden/",
+        "teaser": "http://localhost:4000/assets/images/people/HAP.jpg"
       },{
         "title": "Terri Burton",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/terri-burton/",
+        "url": "http://localhost:4000/people/4-burton-terri/",
         "teaser": "http://localhost:4000/assets/images/people/TB.jpg"
       },{
-        "title": "Yu-Hung Hung",
-        "excerpt":" ","categories": [],
+        "title": "Missy Rung-Blue",
+        "excerpt":"Missy began her career in the for-profit sector and spent 18 years there gaining knowledge and experience with budgeting, project planning, financial reporting, project management, information technology, and team leadership. After 18 years, she decided that she wanted to take these skills to the non-profit sector because she believes that...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/yuhung-hung/",
-        "teaser": "http://localhost:4000/assets/images/people/YHH.jpg"
+        "url": "http://localhost:4000/people/4-rungblue-missy/",
+        "teaser": "http://localhost:4000/assets/images/people/MRW.jpg"
       },]
